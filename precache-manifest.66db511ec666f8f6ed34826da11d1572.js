87f79790e588b0899407a6f31d25dd2bd8389900
oxmad <dima11154@gmail.com>
2019-03-04 00:36:57 +0400

    Updates

--- precache-manifest.66db511ec666f8f6ed34826da11d1572.js
+++ precache-manifest.66db511ec666f8f6ed34826da11d1572.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/GIF-search/static/js/runtime~main.8480ca95.js"
   },
   {
-    "revision": "b4c7397da9741c7c2f4e",
-    "url": "/GIF-search/static/js/main.b4c7397d.chunk.js"
+    "revision": "bff746225f0f829124b3",
+    "url": "/GIF-search/static/js/main.bff74622.chunk.js"
   },
   {
     "revision": "c88c70e5f4ff8bea6fac",
     "url": "/GIF-search/static/js/2.c88c70e5.chunk.js"
   },
   {
-    "revision": "b4c7397da9741c7c2f4e",
+    "revision": "bff746225f0f829124b3",
     "url": "/GIF-search/static/css/main.a55edb2b.chunk.css"
   },
   {
-    "revision": "e43464f3c19a7da48c535d734b4bea2a",
+    "revision": "98d87417484393c3ce56862b1943a44c",
     "url": "/GIF-search/index.html"
   }
 ];
