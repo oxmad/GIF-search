2ac7de42f230502c7ca5f61bad61864ac71bfee1
oxmad <dima11154@gmail.com>
2019-03-04 00:18:12 +0400

    GH-Pages

---
+++ build/precache-manifest.57b20304fa5d9beb36d8f7db263e6c7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8480ca9513b3966fdf95",
+    "url": "/GIF-search/static/js/runtime~main.8480ca95.js"
+  },
+  {
+    "revision": "b4c7397da9741c7c2f4e",
+    "url": "/GIF-search/static/js/main.b4c7397d.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/GIF-search/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "b4c7397da9741c7c2f4e",
+    "url": "/GIF-search/static/css/main.a55edb2b.chunk.css"
+  },
+  {
+    "revision": "e43464f3c19a7da48c535d734b4bea2a",
+    "url": "/GIF-search/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+
+  state = {
+    searchText: '',
+    links: [],
+    btnText: 'Найти!',
+    image: null,
+    err: null
+  }
+
+  // Инпут
+  onInputChange = (e) => {
+    const value = e.target.value;
+    
+    this.setState({
+      err: null,
+      searchText: value,
+      btnText: 'Найти!'
+    });
+  }
+
+  // Поиск гифки
+  searchGIF = (e) => {
+    e.preventDefault();
+
+    const phrase = this.state.searchText;
+
+    if (phrase.trim() !== '') {
+      fetch(`http://api.giphy.com/v1/gifs/search?q=${phrase}&api_key=SccsoCNcduBUF6EiYr9iHKfb90UuRRbQ&limit=25`,
+      {
+        method: "GET"
+      })
+      .then(response => response.json())
+      .then((data) => {
+        let links = [];
+
+        data.data.map((item) => {
+          links.push(item.images.downsized.url);
+
+          return false;
+        });
+
+        this.setState({
+          links,
+          btnText: 'Ещё!'
+        });
+        
+        this.generateImage();
+      });
+    } else {
+      this.setState({ err: 'Введите слово' })
+    }
+  }
+
+  // Генерируем результат
+  generateImage = () => {
+    const { links } = this.state;
+
+    const randomNum = Math.floor(Math.random() * (24 - 0)) + 0;
+    
+    links.map((image, index) => {
+      if (index === randomNum) {
+        this.setState({
+          image: image
+        });
+      };
+
+      return false;
+    });
+  }
+
   render() {
+    const { links, btnText, image, err: error } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <form className="form" onSubmit={ this.searchGIF }>
+            <h1 className="title">Найди свою гифку!</h1>
+            <div className="search-result">
+              { links.length !== 0 ? <img src={image} alt=""/> : null }
+            </div>
+            <div className="search-bar">
+              <input type="text" onChange={this.onInputChange}/>
+              <button type="submit">{ btnText }</button>
+            </div>
+            { error !== null ? <p className="error">{error}</p> : null }
+          </form>
         </header>
       </div>
     );
   }
 }
-
-export default App;
