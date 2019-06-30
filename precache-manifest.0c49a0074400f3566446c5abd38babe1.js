c3c83be8e987d2ff0deb74597a6f6e9dcba753d7
oxmad <dima11154@gmail.com>
2019-06-30 16:00:17 +0400

    Updates

---
+++ precache-manifest.0c49a0074400f3566446c5abd38babe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8480ca9513b3966fdf95",
+    "url": "/GIF-search/static/js/runtime~main.8480ca95.js"
+  },
+  {
+    "revision": "fcd5b12a247f09a17e0d",
+    "url": "/GIF-search/static/js/main.fcd5b12a.chunk.js"
+  },
+  {
+    "revision": "10a97bbd0c853c7923cc",
+    "url": "/GIF-search/static/js/2.10a97bbd.chunk.js"
+  },
+  {
+    "revision": "fcd5b12a247f09a17e0d",
+    "url": "/GIF-search/static/css/main.0b471ed6.chunk.css"
+  },
+  {
+    "revision": "bfd87f3a6f70efa665658f0c947e2060",
+    "url": "/GIF-search/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/GIF-search/precache-manifest.56b1ee194571cef6b5c6258535878ad8.js"
+  "/GIF-search/precache-manifest.0c49a0074400f3566446c5abd38babe1.js"
 );
 
 workbox.clientsClaim();
